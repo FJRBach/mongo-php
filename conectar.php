6dd1ef92569ebcb6ca9574c71bf1be74ed46b241
FJRBach <127007313+FJRBach@users.noreply.github.com>
2024-10-18 00:21:50 -0600

    Update conectar.php

--- conectar.php
+++ conectar.php
@@ -3,7 +3,7 @@ require 'vendor/autoload.php';
 function connectMongoDB() {
     $uri = "mongodb://localhost:27017"; // Cambia si es necesario
     $client = new MongoDB\Client($uri);
-    $database = $client->selectDatabase('bachur'); // Nombre de tu base de datos
+    $database = $client->selectDatabase('bachur'); 
     return $database;
 }
 ?>
